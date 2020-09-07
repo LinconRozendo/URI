e31e2be545f1b1cda55d6de8f772cf86dc8d6d48
Lincon Rozendo <lincon.silva@cear.ufpb.br>
2020-09-07 03:15:38 -0300

    Add files via upload

--- 1014.py
+++ 1014.py
@@ -1,12 +1,6 @@
 #include<stdio.h>
-import math
 
-x = input().split()
-y = input().split()
+x = int(input())
+y = float(input())
 
-x1, y1 = float(x[0]), float(x[1])
-x2, y2 = float(y[0]), float(y[1])
-
-print('%.4f' %(math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))))
-
-
+print('%.3f km/l' %(x / y))
